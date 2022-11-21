20c33bb4a6d7ae615a7574f82f83e6382b39d556
Samat-GIT <seitmuratov.samat@bk.ru>
2022-11-22 01:47:56 +0600

    Initial commit

---
+++ src/main/java/com/samat/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.samat.spring.springboot.spring_data_rest.dao;
+
+
+
+
+import com.samat.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
